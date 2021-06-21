b8eb9eab56151ec7504d0da36f3223838ba2a521
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-22 05:48:02 +0900

    Initial Commit

---
+++ Loco.Kadai.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loco.Kadai.6
+//
+//  Created by 日高隼人 on 2021/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

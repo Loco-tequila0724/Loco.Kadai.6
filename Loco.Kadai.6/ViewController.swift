dc14e708ec6b753fa99df36d0f3f68bb41103411
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-22 17:34:01 +0900

    課題６　完成

--- Loco.Kadai.6/ViewController.swift
+++ Loco.Kadai.6/ViewController.swift
@@ -1,19 +1,36 @@
-//
-//  ViewController.swift
-//  Loco.Kadai.6
-//
-//  Created by 日高隼人 on 2021/06/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak private var slider: UISlider!
+    @IBOutlet weak private var currentNumberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeInitialScreen()
+    }
+    // 初期画面を作る
+    private func makeInitialScreen() {
+        var currentNumber = 0
+        currentNumber = Int.random(in: 1...100)
+        currentNumberLabel.text = String(currentNumber)
+        slider.value = 50
+    }
+    // アラート機能
+    private func makeAndDisplayAlert(message: String) {
+        let alert: UIAlertController = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        let tryAgain = UIAlertAction(title: "再挑戦", style: .default, handler: { [weak self] _ -> Void in self!.makeInitialScreen()
+        })
+
+        alert.addAction(tryAgain)
+        present(alert, animated: true)
     }
 
+    @IBAction private func judgeButton(_ sender: Any) {
+        let sliderValue = String(Int(slider.value))
 
+        if currentNumberLabel.text == sliderValue {
+            makeAndDisplayAlert(message: "あたり！\nあなたの値:\(sliderValue)")
+        } else {
+            makeAndDisplayAlert(message: "はずれ！\nあなたの値:\(sliderValue)")
+        }
+    }
 }
-

3db8808b0fb2c0abf40c0b32ad9b4ec5694f45f1
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-25 21:29:59 +0900

    課題６　フィードバック済

--- Loco.Kadai.6/ViewController.swift
+++ Loco.Kadai.6/ViewController.swift
@@ -3,31 +3,34 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak private var slider: UISlider!
     @IBOutlet weak private var currentNumberLabel: UILabel!
+    private var correctAnswer = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         makeInitialScreen()
     }
     // 初期画面を作る
     private func makeInitialScreen() {
-        var currentNumber = 0
-        currentNumber = Int.random(in: 1...100)
-        currentNumberLabel.text = String(currentNumber)
+        correctAnswer = Int.random(in: 1...100)
+        currentNumberLabel.text = String(correctAnswer)
         slider.value = 50
     }
     // アラート機能
     private func makeAndDisplayAlert(message: String) {
-        let alert: UIAlertController = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
-        let tryAgain = UIAlertAction(title: "再挑戦", style: .default, handler: { [weak self] _ -> Void in self!.makeInitialScreen()
+        let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        let tryAgain = UIAlertAction(
+            title: "再挑戦",
+            style: .default,
+            handler: { [weak self] _ in self?.makeInitialScreen()
         })
-
         alert.addAction(tryAgain)
         present(alert, animated: true)
     }
 
     @IBAction private func judgeButton(_ sender: Any) {
-        let sliderValue = String(Int(slider.value))
+        let sliderValue = Int(slider.value)
 
-        if currentNumberLabel.text == sliderValue {
+        if correctAnswer == sliderValue {
             makeAndDisplayAlert(message: "あたり！\nあなたの値:\(sliderValue)")
         } else {
             makeAndDisplayAlert(message: "はずれ！\nあなたの値:\(sliderValue)")
